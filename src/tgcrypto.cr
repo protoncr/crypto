ad83e4f60a8a77cd86bb49bba360491f812dcb0f
Chris Watson <cawatson1993@gmail.com>
2020-02-23 11:47:32 -0700

    Add CBC require to main file

--- src/tgcrypto.cr
+++ src/tgcrypto.cr
@@ -1,5 +1,6 @@
 require "./tgcrypto/version"
 require "./tgcrypto/aes"
+require "./tgcrypto/cbc"
 
 # TGCrypto is a pure Crystal cryptography library, currently
 # focused on Telegram specific cryptography.
